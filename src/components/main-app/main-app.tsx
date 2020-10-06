9fd27852e86bcf6f509170ecedb609016490ec8e
y.mikhin <y.mikhin@fun-box.ru>
2020-10-06 23:25:51 +0300

    Подготовка окружения и редактирование базового бойлерплейта.

---
+++ src/components/main-app/main-app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MainApp() {
+  return (
+    <div/>
+  );
+}
+
+export default MainApp;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import MainApp from './components/main-app';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
