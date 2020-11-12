a64c05d5b9b0176c05402c41237b5ccfd932870e
y.mikhin <y.mikhin@fun-box.ru>
2020-11-12 13:20:02 +0300

    Добавление хелпера retry.

---
+++ src/app/helpers/retry.ts
@@ -0,0 +1,12 @@
+export default async function retry(fn: () => Promise<void>, retriesLeft = 5, interval = 500): Promise<any> {
+  try {
+    return await fn();
+  } catch (error) {
+    if (retriesLeft > 0) {
+      await new Promise(resolve => setTimeout(resolve, interval));
+
+      return retry(fn, retriesLeft - 1, interval);
+    }
+    throw error;
+  }
+}
