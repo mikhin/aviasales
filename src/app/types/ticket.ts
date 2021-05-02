7da8803ef1524efff9aa439b86dc0efc43893db5
y.mikhin <y.mikhin@fun-box.ru>
2021-05-02 19:23:31 +0300

    Удаление комментариев в типах.

--- src/app/types/ticket.ts
+++ src/app/types/ticket.ts
@@ -1,12 +1,8 @@
 import Segment from "./segment";
 
 type Ticket = {
-  // Цена в рублях
   price: number;
-  // Код авиакомпании (iata)
   carrier: string;
-  // Массив перелётов.
-  // В тестовом задании это всегда поиск "туда-обратно" значит состоит из двух элементов
   segments: Array<Segment>;
 };
 
