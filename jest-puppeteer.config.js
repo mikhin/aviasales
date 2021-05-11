1da072adbb63a668fb0fa77dce398469398da04f
y.mikhin <y.mikhin@fun-box.ru>
2021-05-11 20:07:31 +0300

    Правки код-стайла в конфигах тестов.

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   server: {
-    command: `PORT=3001 npm start`,
+    command: 'PORT=3001 npm start',
     port: 3001,
     launchTimeout: 10000,
     debug: true,
   },
-}
+};
--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: './ts-jest-puppeteer.config.js',
   testRegex: './*\\.test\\.ts$',
   transformIgnorePatterns: [
-    "node_modules/(?!variables/.*)"
-  ]
-}
+    'node_modules/(?!variables/.*)',
+  ],
+};
