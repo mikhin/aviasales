7f9a94821f306c2168ff692d401fee1c5ff70ca7
y.mikhin <y.mikhin@fun-box.ru>
2021-05-31 13:27:51 +0300

    Перевод на https запросы за изображениями перевозчиков.

--- src/app/helpers/get-carrier-logo-url.ts
+++ src/app/helpers/get-carrier-logo-url.ts
@@ -1,2 +1,2 @@
-export const getCarrierLogoUrl = (carrier: string): string => `http://pics.avs.io/99/36/${carrier}.png`;
-export const getCarrierLogo2xUrl = (carrier: string): string => `http://pics.avs.io/198/72/${carrier}.png`;
+export const getCarrierLogoUrl = (carrier: string): string => `https://pics.avs.io/99/36/${carrier}.png`;
+export const getCarrierLogo2xUrl = (carrier: string): string => `https://pics.avs.io/198/72/${carrier}.png`;
